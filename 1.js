d491d22b9a7a81aab985213642ab875e8c071428
nqcuong20 <60700939+nqcuong20@users.noreply.github.com>
2020-04-29 00:26:13 +0700

    add btn back-to-top

--- 1.js
+++ 1.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {
     // Code sẽ viết ở đây
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+          $('.back-to-top').fadeIn('slow');
+        } 
+        else {
+          $('.back-to-top').fadeOut('slow');
+        }
+      });//hàm này là nó cho cái đó nó khi hạ dưới 100px thì hiển thị
     
+      $('.back-to-top').click(function() {
+        
+        console.log("uc");
+        $('html, body').animate({
+            
+          scrollTop: 0
+        }, 1500, 'easeInOutExpo');
+        return false;
+      });
+
+      
 });
